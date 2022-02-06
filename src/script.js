55768bdba1f7ddf5eec3702170737d4fa40573f2
Mael-Kehl <artyworldyah@gmail.com>
2022-02-06 14:02:47 +0100

    Raycaster

--- src/script.js
+++ src/script.js
@@ -1,43 +1,237 @@
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
+//import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+//import * as dat from 'dat.gui'
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
 import gsap from 'gsap'
 
 // Debug
-const gui = new dat.GUI()
+//const gui = new dat.GUI()
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector('canvas.webgl');
+const gltfLoader = new GLTFLoader();
+const pointerClick = new THREE.Vector2();
+let INTERSECTED;
+const pointerMove = new THREE.Vector2();
 
 // Scene
-const scene = new THREE.Scene()
 
-//GLTF Loader
+let camera, renderer, scene, raycasterClick, raycasterMove;
+var sceneMeshes = [];
 
-const gltfLoader = new GLTFLoader();
+function init(){
+    camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+    camera.position.x = 0
+    camera.position.y = 4
+    camera.position.z = 8
 
-//Gsap timeline
+    renderer = new THREE.WebGLRenderer({
+        canvas: canvas,
+        alpha: true,
+    })
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-let tl = gsap.timeline();
+    raycasterClick = new THREE.Raycaster();
+    raycasterMove = new THREE.Raycaster();
 
-//Desk
+
+    document.addEventListener( 'mousedown', onDocumentMouseDown );
+    document.addEventListener( 'mousemove', onPointerMove );
+
+
+    fillScene();
+}
+
+function fillScene(){
+    //Instanciation de la scene
+    scene = new THREE.Scene();
+
+    //Camera 
+    scene.add(camera)
+
+    // Point lumineux
+    const light = new THREE.PointLight(0xFFFFFF, 1.8);
+    light.position.set(3 , 37, 0);
+    /*
+    const light2 = new THREE.DirectionalLight(0xFFFFFF, 1.0);
+    light2.position.set(-500, 250, -200);
+    */
+    scene.add(light)
+
+    //Chargements des modèles gltf
+    gltfLoader.load('deskmesh.gltf', (desk) => {
+
+        /**
+         * Vérifie si "child" est un Mesh, si c'est le cas, 
+         * on lui attribue un id de 0 dans le userData
+         * userData permet de rajouter des champs à notre objets pour le manipuler
+         * On lui donne la possibilité d'intéragir avec des ombres
+         * on l'ajoute au tableau sceneMeshes regroupant tous les Mesh de la scene
+         */
+        desk.scene.traverse(function (child) {
+            if (child.isMesh) {
+                let m = child;
+                m.userData.id = 0;
+                m.receiveShadow = true;
+                m.castShadow = true;
+                sceneMeshes.push(m);
+            }
+        })
+
+        desk.scene.scale.set(0.1,0.1,0.1)
+        desk.scene.position.y += 2;
+
+        scene.add(desk.scene);
+
+        //Loading left screen's gltf model
+        gltfLoader.load('screen1mesh.gltf', (leftscreen) => {
+
+            leftscreen.scene.traverse(function (child) {
+                if (child.isMesh) {
+                    let m = child;
+                    m.userData.id = 1;
+                    m.receiveShadow = true;
+                    m.castShadow = true;
+                    sceneMeshes.push(m);
+                }
+            })
+
+            leftscreen.scene.scale.set(0.1,0.1,0.1)
+            leftscreen.scene.position.y += 2;
+        
+            scene.add(leftscreen.scene);
+            sceneMeshes.push(leftscreen.scene);
+            
+            //Loading right screen's gltf model
+            gltfLoader.load('screen2mesh.gltf', (rightscreen) => {
+
+                rightscreen.scene.traverse(function (child){
+                    if (child.isMesh) {
+                        let m = child;
+                        m.userData.id = 2;
+                        m.receiveShadow = true;
+                        m.castShadow = true;
+                        sceneMeshes.push(m);
+                    }
+                });
+                rightscreen.scene.scale.set(0.1,0.1,0.1)
+                rightscreen.scene.position.y += 2;
+
+                scene.add(rightscreen.scene)
+                sceneMeshes.push(rightscreen.scene)
+            
+                //Animating the 3 elements at the same time
+                gsap.to(desk.scene.rotation, {y: 4.8, duration: 1})
+                gsap.to(leftscreen.scene.rotation, {y: 4.8, duration: 1})
+                gsap.to(rightscreen.scene.rotation, {y: 4.8, duration: 1})
+            })
+        })
+    })
+    console.log(sceneMeshes);
+}
+
+function onPointerMove( event ) {
+
+    pointerMove.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    pointerMove.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    
+    const intersectsMove = raycasterMove.intersectObjects( sceneMeshes );
+
+
+    if (intersectsMove.length > 0) {
+
+        if (intersectsMove[0].object.userData.id == 1) {
+            sceneMeshes.forEach(mesh => {
+                if (mesh.userData.id == 1) {
+                    gsap.to(mesh.position, {z:0.05, duration: 0.5})
+                }
+            });
+        }
+        else{
+            sceneMeshes.forEach(mesh => {
+                if (mesh.userData.id == 1) {
+                    gsap.to(mesh.position, {z:0.0, duration: 0.5})
+                }
+            });
+        }
+        if(intersectsMove[0].object.userData.id == 2){
+            sceneMeshes.forEach(mesh => {
+                if (mesh.userData.id == 2) {
+                    gsap.to(mesh.position, {z:0.05, duration: 0.5})
+                }
+            });
+        }
+        else{
+            sceneMeshes.forEach(mesh => {
+                if (mesh.userData.id == 2) {
+                    gsap.to(mesh.position, {z:0.0, duration: 0.5})
+                }
+            });
+        }
+
+    }
+
+
+}
+let zoomedScreenLeft = false;
+let zoomedScreenRight = false;
+
+function onDocumentMouseDown( event ) {
+    pointerClick.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    pointerClick.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    // Debug : console.log(pointer.x, pointer.y)
+
+    raycasterClick.setFromCamera(pointerClick, camera);
+
+    const intersects = raycasterClick.intersectObjects( sceneMeshes );
+
+    //.set(1.02, 1.02, 1.02);
+
+    if (intersects.length > 0) {
+        if (intersects[0].object.userData.id == 0) {
+            console.log("Bureau")
+        }
+        //If we clicked on the left screen
+        else if (intersects[0].object.userData.id == 1) {
+            
+            //If it isn't zoomed, then we zoom in
+            if (!zoomedScreenLeft) {
+                gsap.to(camera.position, {z: -0.15,y: 4.63,x:-0.5, duration: 1});
+                gsap.to(camera.rotation, {x: 0, y:0.3, duration: 1});
+                zoomedScreenLeft = true;
+            }
+            //If we already zoomed in, we zoom out
+            else{
+                gsap.to(camera.rotation, {x: -0.5, y:0, duration: 1});
+                gsap.to(camera.position, {z: 2,y: 5,x:0, duration: 1});
+                zoomedScreenLeft = false;
+            }
+            console.log("gauche")
+        }
+        else if (intersects[0].object.userData.id == 2) {
+            console.log("droite")
+            if (!zoomedScreenRight) {
+                gsap.to(camera.position, {z: -0.25,y: 4.63,x:0.65, duration: 1});
+                gsap.to(camera.rotation, {x: 0, y:-0.15, duration: 1});
+                zoomedScreenRight = true;
+            }
+            //If we already zoomed in, we zoom out
+            else{
+                gsap.to(camera.rotation, {x: -0.5, y:0, duration: 1});
+                gsap.to(camera.position, {z: 2,y: 5,x:0, duration: 1});
+                zoomedScreenRight = false;
+            }
+        }
+
+        //Debug : console.log(intersects)
+    }
+}
 
 const nextbutton = document.getElementById("next");
 const backbutton = document.getElementById("back");
 const intro = document.querySelector(".intro-container");
-
-gltfLoader.load('desk.gltf', (gltf) => {
-    gltf.scene.scale.set(0.1,0.1,0.1)
-    gltf.scene.position.y += 2;
-
-    scene.add(gltf.scene);
-
-    tl.to(gltf.scene.rotation, {y: 4.8, duration: 1})
-})
-
-
 
 let count = 0
 
@@ -52,23 +246,12 @@ nextbutton.addEventListener('click', (e) => {
             nextbutton.innerHTML = "Continue"
             break;
         }  
-        case 1: //Zoom sur l'écran de gauche
+        case 1:
         {
-            gsap.to(camera.position, {z: -0.15,y: 4.63,x:-0.5, duration: 1});
-            gsap.to(camera.rotation, {x: 0, y:0.3, duration: 1});
-            setTimeout(() => {
-                intro.style.display = "block";
-                fade();
-                setTimeout(()=>{
-                    type();
-                }, 1000);
-            }, 1100);  
             break;
         }  
         case 2: //Zoom sur l'écran de droite
         {
-            gsap.to(camera.position, {z: -0.25,y: 4.63,x:0.65, duration: 1});
-            gsap.to(camera.rotation, {x: 0, y:-0.15, duration: 1});
             break;
         }  
             
@@ -92,15 +275,10 @@ backbutton.addEventListener('click', (e) => {
             
         case 2: //Zoom sur l'écran de gauche
         {
-            gsap.to(camera.rotation, {x: -0.5, y:0, duration: 1});
-            gsap.to(camera.position, {z: 2,y: 5,x:0, duration: 1});
-            intro.style.display = "none";
             break;
         }
         case 3: //Zoom sur l'écran de droite
         {
-            gsap.to(camera.position, {z: -0.15,y: 4.63,x:-0.5, duration: 1});
-            gsap.to(camera.rotation, {x: 0, y:0.3, duration: 1});
             break;
         }
     
@@ -108,27 +286,7 @@ backbutton.addEventListener('click', (e) => {
             break;
     }
     count--;
-    console.log("genial")
 })
-
-
-
-
-// Lights
-
-const ambientLight = new THREE.AmbientLight(0xffffff, 1)
-ambientLight.position.x = 2
-ambientLight.position.y = 3
-ambientLight.position.z = 4
-scene.add(ambientLight)
-
-const light = new THREE.DirectionalLight(0xFFFFFF, 1.0);
-light.position.set(200, 400, 500);
-const light2 = new THREE.DirectionalLight(0xFFFFFF, 1.0);
-light2.position.set(-500, 250, -200);
-
-scene.add(light)
-scene.add(light2)
 
 
 /**
@@ -154,51 +312,25 @@ window.addEventListener('resize', () =>
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 0
-camera.position.y = 4
-camera.position.z = 8
-scene.add(camera)
-
-// Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
-
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    alpha: true,
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-/**
- * Animate
- */
 
 const clock = new THREE.Clock()
 
-const tick = () =>
+//Animating the scene
+const animate = () =>
 {
-
-    const elapsedTime = clock.getElapsedTime()
-
-    // Update objects
-
-    // Update Orbital Controls
-    // controls.update()
-
-    // Render
-    renderer.render(scene, camera)
-
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+    render();
+    window.requestAnimationFrame(animate)
 }
 
-tick()
+//render of the scene
+function render(){
+
+    raycasterMove.setFromCamera( pointerMove, camera );
+    var intersects = raycasterClick.intersectObjects(scene.children, true); 
+
+    renderer.render(scene, camera);
+}
+
+
+init();
+animate()
