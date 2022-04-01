02bc4e8a271a85e330f78fcf8d3827e8f9fb4e15
Mael-Kehl <artyworldyah@gmail.com>
2022-04-01 18:22:49 +0200

    responsive for PC

--- src/animations.js
+++ src/animations.js
@@ -2,13 +2,17 @@ import './style.css';
 
 const slider = document.querySelector('.slider');
 const slides = Array.from(document.querySelectorAll('.slide'));
+const arrowNext = document.getElementById("arrow-next");
+const arrowPrev = document.getElementById("arrow-prev");
 
 let isDragging = false,
     startPos = 0,
     currentTranslate = 0,
     prevTranslate = 0,
     animationID = 0,
-    currentIndex = 0;
+    currentIndex = 1;
+
+setPositionByIndex();
 
 slides.forEach((slide, index) => {
    //const slideImage = slide.querySelector('img');
@@ -57,8 +61,28 @@ function touchEnd(){
     setPositionByIndex();
 
     slider.classList.remove('grabbing');
-
 }
+
+arrowNext.addEventListener('click', () => {
+    if(currentIndex < slides.length - 1) currentIndex += 1;
+    setPositionByIndex();
+});
+
+arrowPrev.addEventListener('click', () => {
+    if(currentIndex > 0) currentIndex -= 1;
+    setPositionByIndex();
+});
+
+document.addEventListener('keydown', (e) => {
+    if (e.code === "ArrowRight"){ 
+        if(currentIndex < slides.length - 1) currentIndex += 1;
+        setPositionByIndex();
+    }
+    else if (e.code === "ArrowLeft"){
+        if(currentIndex > 0) currentIndex -= 1;
+        setPositionByIndex();
+    }
+});
 
 function touchMove(event){
     if(isDragging){
